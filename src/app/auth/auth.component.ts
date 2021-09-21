2a9233388259952da372710fc50a63e3960b85a2
Shourya Dey <shourya.dey@sisuhealthgroup.com>
2021-09-21 12:32:06 +0530

    Updated routing after authentication

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
 
@@ -14,7 +15,7 @@ export class AuthComponent implements OnInit {
   error: string = null;
 
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
@@ -43,7 +44,7 @@ export class AuthComponent implements OnInit {
     authObs.subscribe(resData => {
       console.log(resData);
       this.isLoading = false;
-
+      this.router.navigate(['/recipes']);
     }, errMessage => {
       console.log(errMessage);
       this.error = errMessage;
